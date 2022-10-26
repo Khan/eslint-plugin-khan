e4839f17612e426a30b498077b748d3e9bc8e7c2
Jeff Yates <jeff@khanacademy.org>
2022-10-26 14:42:48 -0500

    Fix JSON format

--- test/sync-tag_test.js
+++ test/sync-tag_test.js
@@ -13,27 +13,84 @@ util.execSync = command => {
     console.log("execSync mock --------");
     if (command.includes("filea")) {
         const json = JSON.stringify({
-            version: "2.2.3",
+            version: "4.0.0",
             launchString: "",
-            items: [],
+            files: {},
         });
         return json;
     }
     if (command.includes("filex")) {
         return JSON.stringify({
-            version: "2.2.3",
+            version: "4.0.0",
             launchString: "",
-            items: [
-                {
-                    type: "violation",
-                    sourceFile: "filex",
-                    sourceLine: 2,
-                    targetFile: "filey",
-                    targetLine: 23,
-                    message: `filex:15 Updating checksum for sync-tag 'foo-bar' referencing 'filey:23' from No checksum to 1424803960.`,
-                    fix: "// sync-start:foo-bar 1424803960 filey",
+            files: {
+                filex: {
+                    items: [
+                        {
+                            reason:
+                                "Looks like you changed the target content for sync-tag 'foo-bar' in 'filex:2'. Make sure you've made the parallel changes in the source file, if necessary (12352 != 249234014)",
+                            location: {
+                                line: 2,
+                            },
+                            code: "violation",
+                            fix: {
+                                type: "replace",
+                                line: 2,
+                                description:
+                                    "Updated checksum for sync-tag 'foo-bar' referencing 'filex:2' from 12352 to 249234014.",
+                                declaration: "// sync-start:foo-bar filey",
+                                text: "// sync-start:foo-bar 1424803960 filey",
+                            },
+                        },
+                    ],
                 },
-            ],
+            },
+        });
+    }
+    if (command.includes("file_delete_tag")) {
+        return JSON.stringify({
+            version: "4.0.0",
+            launchString: "",
+            files: {
+                file_delete_tag: {
+                    items: [
+                        {
+                            reason: "Duplicate target for sync-tag 'foo-bar'",
+                            location: {
+                                line: 2,
+                            },
+                            code: "violation",
+                            fix: {
+                                type: "delete",
+                                line: 3,
+                                description:
+                                    "Removed duplicate target for sync-tag 'foo-bar'",
+                                declaration: "// sync-start:foo-bar filey",
+                            },
+                        },
+                    ],
+                },
+            },
+        });
+    }
+    if (command.includes("file_unfixable")) {
+        return JSON.stringify({
+            version: "4.0.0",
+            launchString: "",
+            files: {
+                file_unfixable: {
+                    items: [
+                        {
+                            reason:
+                                "Sync-start for 'foo-bar' points to 'filey', which does not exist or is a directory",
+                            location: {
+                                line: 2,
+                            },
+                            code: "file-does-not-exist",
+                        },
+                    ],
+                },
+            },
         });
     }
     return "";
@@ -43,7 +100,7 @@ ruleTester.run("sync-tag", rule, {
     valid: [
         {
             code:
-                "// sync-start:foo-bar 1424803960 fileb\nconst FooBar = 'foobar';\n// sync-end:foobar",
+                "// sync-start:foo-bar 1424803960 fileb\nconst FooBar = 'foobar';\n// sync-end:foo-bar",
             filename: "filea",
             options: [
                 {
@@ -54,7 +111,7 @@ ruleTester.run("sync-tag", rule, {
         },
         {
             code:
-                "// sync-start:foo-bar 1424803960 fileb\nconst FooBar = 'foobar';\n// sync-end:foobar",
+                "// sync-start:foo-bar 1424803960 fileb\nconst FooBar = 'foobar';\n// sync-end:foo-bar",
             filename: "filea",
             options: [
                 {
@@ -70,16 +127,57 @@ ruleTester.run("sync-tag", rule, {
             code: `
                 // sync-start:foo-bar filey
                 const FooBar = 'foobar';
-                // sync-end:foobar`,
+                // sync-end:foo-bar`,
             filename: "filex",
             errors: [
-                "filex:15 Updating checksum for sync-tag 'foo-bar' referencing 'filey:23' from No checksum to 1424803960. " +
-                    "If necessary, check the sync-tag target and make relevant changes before updating the checksum.",
+                "Looks like you changed the target content for sync-tag 'foo-bar' in 'filex:2'. Make sure you've made the parallel changes in the source file, if necessary (12352 != 249234014)",
             ],
             output: `
                 // sync-start:foo-bar 1424803960 filey
                 const FooBar = 'foobar';
-                // sync-end:foobar`,
+                // sync-end:foo-bar`,
+            options: [
+                {
+                    ignoreFiles: ["lint_blacklist.txt"],
+                    rootDir: "/Users/nyancat/project",
+                },
+            ],
+        },
+        {
+            code: `
+                // sync-start:foo-bar 1424803960 filey
+                // sync-start:foo-bar 1424803960 filey
+                const FooBar = 'foobar';
+                // sync-end:foo-bar`,
+            filename: "file_delete_tag",
+            errors: ["Duplicate target for sync-tag 'foo-bar'"],
+            // Prettier is removing whitespace within backticks, so I had to add
+            // the newlines manually.
+            output: `
+                // sync-start:foo-bar 1424803960 filey\n                \n                const FooBar = 'foobar';
+                // sync-end:foo-bar`,
+            options: [
+                {
+                    ignoreFiles: ["lint_blacklist.txt"],
+                    rootDir: "/Users/nyancat/project",
+                },
+            ],
+        },
+        {
+            code: `
+                // sync-start:foo-bar 1424803960 filey
+                const FooBar = 'foobar';
+                // sync-end:foo-bar`,
+            filename: "file_unfixable",
+            errors: [
+                "Sync-start for 'foo-bar' points to 'filey', which does not exist or is a directory",
+            ],
+            // Prettier is removing whitespace within backticks, so I had to add
+            // the newlines manually.
+            output: `
+                // sync-start:foo-bar 1424803960 filey
+                const FooBar = 'foobar';
+                // sync-end:foo-bar`,
             options: [
                 {
                     ignoreFiles: ["lint_blacklist.txt"],
